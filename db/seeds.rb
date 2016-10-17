633a0ceba59507b3732a2ff0bd592b0b3b145060
cameroncogan <camycogan@gmail.com>
2016-10-17 15:57:18 -0400

    fixing local files

---
+++ db/seeds.rb
@@ -0,0 +1,30 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Card.create!(name: 'The Fool', image_path: '00_fool.jpg')
+Card.create!(name: 'The Magician', image_path: '01_magician.jpg')
+Card.create!(name: 'The High Priestess', image_path: '02_high_priestess.jpg')
+Card.create!(name: 'The Empress', image_path: '03_empress.jpg')
+Card.create!(name: 'The Emperor', image_path: '04_emperor.jpg')
+Card.create!(name: 'The Hierophant', image_path: '05_hierophant.jpg')
+Card.create!(name: 'The Lovers', image_path: '06_lovers.jpg')
+Card.create!(name: 'The Chariot', image_path: '07_chariot.jpg')
+Card.create!(name: 'Strength', image_path: '08_strength.jpg')
+Card.create!(name: 'The Hermit', image_path: '09_hermit.jpg')
+Card.create!(name: 'The Wheel of Fortune', image_path: '10_wheel_of_fortune.jpg')
+Card.create!(name: 'Justice', image_path: '11_justice.jpg')
+Card.create!(name: 'The Hanged Man', image_path: '12_the_hanged_man.jpg')
+Card.create!(name: 'Death', image_path: '13_death.jpg')
+Card.create!(name: 'Temperance', image_path: '14_temperance.jpg')
+Card.create!(name: 'The Devil', image_path: '15_devil.jpg')
+Card.create!(name: 'The Tower', image_path: '16_tower.jpg')
+Card.create!(name: 'The Star', image_path: '17_star.jpg')
+Card.create!(name: 'The Moon', image_path: '18_moon.jpg')
+Card.create!(name: 'The Sun', image_path: '19_sun.jpg')
+Card.create!(name: 'Judgement', image_path: '20_judgement.jpg')
+Card.create!(name: 'The World', image_path: '21_the_world.jpg')
