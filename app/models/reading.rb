3b71672a4ba998337f25f07cb206dce4a970da7f
cameroncogan <camycogan@gmail.com>
2016-10-18 19:37:50 -0400

    adds reading table and model

---
+++ app/models/reading.rb
@@ -0,0 +1,7 @@
+class Reading < ActiveRecord::Base
+  has_many :reveals
+  has_many :cards, through: :reveals
+  belongs_to :user
+  
+  validates :user, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018154812) do
+ActiveRecord::Schema.define(version: 20161018210812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20161018154812) do
     t.text     "outcome",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "readings", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_readings_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
