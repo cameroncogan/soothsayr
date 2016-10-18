58e4c8e1014977ccd5664b5343ff54efc2dbb14c
cameroncogan <camycogan@gmail.com>
2016-10-18 11:58:32 -0400

    validates card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,14 @@
 class Card < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :image_path, presence: true
+  validates :signifier, presence: true
+  validates :crossing, presence: true
+  validates :foundation, presence: true
+  validates :past, presence: true
+  validates :crown, presence: true
+  validates :future, presence: true
+  validates :emotions, presence: true
+  validates :forces, presence: true
+  validates :desires, presence: true
+  validates :outcome, presence: true
 end
