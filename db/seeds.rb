40dd751bbfbaaa004670303dfedf2e54c53ff538
cameroncogan <camycogan@gmail.com>
2016-11-01 08:56:33 -0400

    updates seed file with first batch of new cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,154 @@
 Card.create!(
+  name: 'The Fool',
+  image_path: '00_fool',
+  situation: "The Fool represents the moment before a beginning. In this context, your mind dwells on a metaphorical or physical journey you will take, or perhaps you are thinking about making a fresh start.",
+  danger: "The Fool represents the moment before a beginning. In this position it urges you to avoid beginning again. Your path is set. Don't stray from it now.",
+  safety: "The Fool represents the moment before a beginning. At this juncture of the spread it suggests that a new start is required, or that you take a fresh look through the innocent eyes of a beginner.",
+  outcome: "The Fool represents the moment before a beginning. As the outcome card, it implies that you'll get another shot at this. Life and learning are circular. You'll walk this road again."
+)
+
+Card.create!(
+  name: 'The Magician',
+  image_path: '01_magician',
+  situation: 'The Magician represents creative forces brought to bear in the world. As the situation, it suggests that you need to make some magic in your life.',
+  danger: "The Magician represents creative forces brought to bear in the world. In this context, though, creativity should be avoided. Don't rely on your own prowess. Seek help from others.",
+  safety: "The Magician represents creative forces brought to bear in the world. In this position to urges self-reliance and using your knowledge to make your own path forward.",
+  outcome: "The Magician represents creative forces brought to bear in the world. As the outcome, it suggests that your struggle will grant you knowledge that may serve for a lifetime."
+)
+
+Card.create!(
+  name: 'The High Priestess',
+  image_path: '02_high_priestess',
+  situation: "The High Priestess represents understanding through direct experience. In this context, your mind turns to knowledge you seek or sought, the kind of truth you only learn the hard way.",
+  danger: "The High Priestess represents understanding through direct experience. Revealed here, it urges you not to take shortcuts. Wisdom must be earned, it can't be imparted.",
+  safety: "The High Priestess represents understanding through direct experience. The card suggests that to find wisdom in your situation, you need to be willing to receive it, and put the work in.",
+  outcome: "The High Priestess represents understanding through direct experience. It's meaning as the Outcome is plain. You will learn from this experience, but only after passing through the fire."
+)
+
+Card.create!(
+  name: 'The Empress',
+  image_path: '03_empress',
+  situation: "The Empress represents abundance and fertility. In this context, your thoughts may turn to what you have, or what you wish to have. The connotation is material as well as feminine.",
+  danger: "The Empress represents abundance and fertility. Seen here as a warning, the card urges you to look beyond possessions, beyond mere wealth. Reject comfort.",
+  safety: "The Empress represents abundance and fertility. There is truth in gold. Seen here, the card directs you to build security for yourself in the material world.",
+  outcome: "The Empress represents abundance and fertility. If you're female, this is a strong outcome card, representing success. If you're male, this card predicts a female presence guiding you forward."
+)
+
+Card.create!(
+  name: 'The Emperor',
+  image_path: '04_emperor',
+  situation: 'The Emperor represents order and stability. It may appear here because there is stagnating rigidity in your life, or because your life has been thrown into disarray.',
+  danger: "The Emperor represents order and stability. In this context, you are warned against structure. Break free. Roll the dice.",
+  safety: "The Emperor represents order and stability. Appearing here, the card urges you to organize better, to be the Emperor of your own situation.",
+  outcome: "The Emperor represents order and stability. As the Outcome, it suggests that things will make it to place of constancy, for well or ill."
+)
+
+Card.create!(
+  name: 'The Hierophant',
+  image_path: '05_hierophant',
+  situation: "The Hierophant represents faith. Perhaps your mind dwells on your religion or lack thereof, but you may also consider how your beliefs relate to your actions.",
+  danger: "The Hierophant represents faith. Appearing here, it warns you against giving your trust to things you don't understand, or holding contrary beliefs. Keep your eyes open.",
+  safety: "The Hierophant represents faith. In this context, you are called to believe. There is great power all around you. Channel what is old, what has been kept.",
+  outcome: "The Hierophant represents faith. The card appearing here means that your beliefs will be justified in the end. You have the right of it."
+)
+
+Card.create!(
+  name: 'The Lovers',
+  image_path: '06_lovers',
+  situation: "The Lovers represents instinctive decisions. Maybe romance is on your mind, but just as likely you struggle with taking action. Is that why you're here?",
+  danger: "The Lovers represents instinctive decisions. In this context, it urges pushing away from restraints. Don't let yourself be tied down, by love or by anything.",
+  safety: "The Lovers represents instinctive decisions. Appearing here, it suggests that your follow your heart, not your head. You won't be led astray.",
+  outcome: "The Lovers represents instinctive decisions. This card as the Outcome suggests that you'll reach your goal without even trying."
+)
+
+Card.create!(
+  name: 'The Chariot',
+  image_path: '07_chariot',
+  situation: "The Chariot represents overcoming adversity. Something is blocking your path, all right. You are the Charioteer, and the road is perilous.",
+  danger: "The Chariot represents overcoming adversity. Appearing here, the card warns against a direct approach. You can't just charge through this situation.",
+  safety: "The Chariot represents overcoming adversity. In this context it suggests a direct approach. Charge right at your problem, head on, no matter what happens.",
+  outcome: "The Chariot represents overcoming adversity. This is a positive Outcome card. You'll leave this situation in the dust, soon enough."
+)
+
+Card.create!(
+  name: 'Strength',
+  image_path: '08_strength',
+  situation: "Strength represents calm control. You have come here because you are wrestling with a lion. You want to know how to tame it.",
+  danger: "Strength represents calm control. In this position, serenity is decried. Become the lion. Let your wild side rule you.",
+  safety: "Strength represents calm control. What is needed here is thoughtful, deliberate understanding. Honey will conquer the lion.",
+  outcome: "Strength represents calm control. As the outcome, it suggests achieving resolution through calming yourself and the beast your struggle with."
+)
+
+Card.create!(
+  name: 'The Hermit',
+  image_path: '09_hermit',
+  situation: "The Hermit represents truth found in solitude. As the situation, perhaps you feel lonely--or perhaps you want to get away from it all.",
+  danger: "The Hermit represents truth found in solitude. Revealed here, it warns you against going it alone, of straying from others. You need connection.",
+  safety: "The Hermit represents truth found in solitude. In this context, the card urges you to step back from it all. There is power in walking alone.",
+  outcome: "The Hermit represents truth found in solitude. As the Outcome, expect to spend some time alone, and to come by your solution thereby."
+)
+
+Card.create!(
+  name: 'The Wheel of Fortune',
+  image_path: '10_wheel_of_fortune',
+  situation: "The Wheel of Fortune represents randomness. Something occurred that you had no control over, and you dwell upon it now.",
+  danger: "The Wheel of Fortune represents randomness. In this situation, you are warned against trusting to luck. Try to control every variable you can.",
+  safety: "The Wheel of Fortune represents randomness. Appearing here, the card urges you to spin the wheel, to risk what you have on the altar of chance.",
+  outcome: "The Wheel of Fortune represents randomness. This is neither positive or negative as an Outcome; rather, the resolution will be unforseen and outside your control."
+)
+
+Card.create!(
+  name: 'Justice',
+  image_path: '11_justice',
+  situation: "Justice represents the consequences of your actions. You arrived here by your own hand. Think on how your choices have created this situation.",
+  danger: "Justice represents the consequences of your actions. In this position, it warns you to avoid taking responsibility. If you move fast enough, you can still get ahead of this thing.",
+  safety: "Justice represents the consequences of your actions. Here, it suggests that you step up and accept responsibility for your part in this situation, and look within before casting blame.",
+  outcome: "Justice represents the consequences of your actions. As the Outcome, it suggests that karma will come full circle in the end. The universe will sort it out."
+)
+
+Card.create!(
   name: 'The Hanged Man',
   image_path: '12_hanged_man',
   situation: "The Hanged Man represents sacrifice, surrender, and letting go. As it identifies your situation, perhaps you've been asked to step out of your comfort zone, or to give of yourself.",
   danger: "The Hanged Man represents sacrifice, surrender, and letting go. In this context, the card suggests that you don't make sacrifices. Don't surrender. Look out for yourself, first.",
   safety: "The Hanged Man represents sacrifice, surrender, and letting go. Appearing here, it suggests that the way forward is to turn your viewpoint upside down, and take a fresh look at what you see.",
-  outcome: "The Hanged Man represents sacrifice, surrender, and letting go. You will be uncomfortable before this is over, but hanging upside down will change how you see things.",
+  outcome: "The Hanged Man represents sacrifice, surrender, and letting go. You will be uncomfortable before this is over, but hanging upside down will change how you see things."
+)
+
+Card.create!(
+  name: 'Death',
+  image_path: '13_death',
+  situation: "Death represents transformation. Your mind dwells on something ending, on something changing, and what it means for you.",
+  danger: "Death represents transformation. In this context, you are warned against letting things end. Not all change is for the better.",
+  safety: "Death represents transformation. Appearing here, the card suggests that change is needed to move forward. Out with the old.",
+  outcome: "Death represents transformation. Something will come to an end, here, but something vital will arise from that ending."
+)
+
+Card.create!(
+  name: 'Temperance',
+  image_path: '14_temperance',
+  situation: "Temperance represents the right thing at the right time. You are concerned with finding either balance or perfection in something.",
+  danger: "Temperance represents the right thing at the right time. Appearing here, it calls on you to ignore synchronicity and personal balance. Rage forward, unyielding.",
+  safety: "Temperance represents the right thing at the right time. The card suggests centering yourself, acting with grace and intuition, and being confident.",
+  outcome: "Temperance represents the right thing at the right time. It's a powerful outcome, suggesting that the cosmos will provide for you just when it is needed."
+)
+
+Card.create!(
+  name: 'The Devil',
+  image_path: '15_devil',
+  situation: 'The Devil represents addiction, enthrallment. Is there something in your life that has more power over you than it should?',
+  danger: "The Devil represents addiction, enthrallment. Appearing here, the card warns against letting yourself be enslaved by anything or anyone.",
+  safety: "The Devil represents addiction, enthrallment. In this context, it urges you to go deeper down the rabbit hole, to chase that which captivates you.",
+  outcome: "The Devil represents addiction, enthrallment. This is a negative Outcome, suggesting you will be ensnared by what you most desire."
+)
+
+Card.create!(
+  name: 'The Tower',
+  image_path: '16_tower',
+  situation: "The Tower represents sudden ruination. As the Situation, it sounds like the walls are crumbling around you.",
+  danger: "The Tower represents sudden ruination. Appearing here, you are warned to think carefully on your actions. How could they hurt you?",
+  safety: "The Tower represents sudden ruination. Here, the card tells that the only way forward is destruction. Sweep all the pieces off the board.",
+  outcome: "The Tower represents sudden ruination. It is a very negative Outcome card. Everything's going to come crashing down around you, poor thing."
 )
 
 Card.create!(
@@ -13,7 +157,43 @@ Card.create!(
   situation: "The Star represents serenity and guidance. You have come to this reading to seek council, to find peace.",
   danger: "The Star represents serenity and guidance. In this context, the card urges you not to trust to hope or the good will of the cosmos. You're in this alone.",
   safety: "The Star represents serenity and guidance. The card suggests that you put your trust in fate. No matter how cloudy the sky, the stars are out there.",
-  outcome: "The Star represents serenity and guidance. It is very positive in the outcome position. Karma has it's eyes on you, and will bear you up. Don't be worried.",
+  outcome: "The Star represents serenity and guidance. It is very positive in the outcome position. Karma has it's eyes on you, and will bear you up. Don't be worried."
+)
+
+Card.create!(
+  name: 'The Moon',
+  image_path: '18_moon',
+  situation: "The Moon represents deception, illusion, and dreams. This situation may not be as straightforward as it appears. Be wary of what you see.",
+  danger: "The Moon represents deception, illusion, and dreams. Here, it warns against the same. Don't be taken in by liars. What seems true may be a facade.",
+  safety: "The Moon represents deception, illusion, and dreams. There is power in mystery. Appearing here, you are urged to ignore truth and embrace artifice.",
+  outcome: "The Moon represents deception, illusion, and dreams. Appearing here, it suggests a hazy conclusion. You won't understand what happens."
+)
+
+Card.create!(
+  name: 'The Sun',
+  image_path: '19_sun',
+  situation: "The Sun represents clarity. As the Situation, it suggests that you're in the dark, and need a light.",
+  danger: "The Sun represents clarity. Here, it warns against the truth. You may not like what you find out.",
+  safety: "The Sun represents clarity. This draw suggests that shining a light on the facts will reveal what you need.",
+  outcome: "The Sun represents clarity. Positioned here, this card predicts you finding truth in the end, and being happy with that."
+)
+
+Card.create!(
+  name: 'Judgement',
+  image_path: '20_judgement',
+  situation: "Judgement represents heading a call. As the Situation, it implies that you feel pointed towards something, and wonder whether to pursue it.",
+  danger: "Judgement represents heading a call. In this context, the card suggests caution. Even if everything points to a way forward, do not walk it. Find your own way.",
+  safety: "Judgement represents heading a call. Positioned here, it insists that you DO whatever is compelling you. Get off the fence. Follow the lead.",
+  outcome: "Judgement represents heading a call. As the Outcome, it signifies rebirth and new vision are in store. That doesn't sound so bad."
+)
+
+Card.create!(
+  name: 'The World',
+  image_path: '21_world',
+  situation: "The World signifies success. Even as the Situation, it has a positive connotation. Your thoughts linger on your accomplishments.",
+  danger: "The World signifies success. In this context, it represents the end of the road. Have you really reached it? Push further, grasp for more.",
+  safety: "The World signifies success. Appearing here, it proclaims that you have arrived. Don't keep struggling with this. Your solution is good enough.",
+  outcome: "The World signifies success. It is the most positive Outcome. Take a deep breath. You'll have what you're after."
 )
 
 Card.create!(
